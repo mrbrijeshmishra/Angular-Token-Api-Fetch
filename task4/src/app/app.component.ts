af3a1442f4d2f8227a2535b22d06c4a09592f4ed
Brijesh Mishra <120309729+mrbrijeshmishra@users.noreply.github.com>
2024-03-12 23:55:57 +0530

    Add files via upload

---
+++ task4/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { UserDataService } from './services/user-data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Api Fetching';
+  users: any;
+  constructor(private userData: UserDataService) {
+    this.userData.users().subscribe(data =>{ this.users = data;
+    // console.log(data);
+  })
+  };
+}
